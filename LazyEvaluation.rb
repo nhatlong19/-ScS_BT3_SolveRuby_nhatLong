b5de7faeab85fddbd60c8a48919087bb68fa7cd5
YOUR NAME <YOUR@EMAIL.com>
2021-11-30 16:22:54 +0700

    LazyEvaluation

---
+++ LazyEvaluation.rb
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+require 'prime'
+
+require 'prime'
+primes = []
+puts "[#{Prime.each.lazy.select{|x| x.to_s == x.to_s.reverse}.first(gets.to_i).join(", ")}]"
